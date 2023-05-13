79e960ef9f1e9ba2b40c5194a1286b5de9692694
bingling_sama <bingling_sama@qq.com>
2023-05-13 23:57:37 +0800

    ✨ feat(web history): change web history
    change from WebHashHistory to WebHistory

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import '@sakun/system.css'
 import './style.css'
 import App from './App.vue'
@@ -13,7 +13,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
