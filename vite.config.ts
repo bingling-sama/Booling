7ad74c3c49152eca1403b9183037d87f831a6f0a
bingling-sama <bingling_sama@qq.com>
2023-11-04 22:43:25 +0800

    ✨ feat(imagemin): minimize images

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import viteCompression from 'vite-plugin-compression'
+import imagemin from 'unplugin-imagemin/vite'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),viteCompression()]
+  plugins: [vue({}), viteCompression({}), imagemin({})]
 })
