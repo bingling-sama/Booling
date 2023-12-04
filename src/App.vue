f3b54a9c64f0393f33ebcca201ac839e1312c353
bingling-sama <bingling_sama@qq.com>
2023-12-05 00:20:29 +0800

    🎈 perf(style): improve css style

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
-  <div class="window">
-    <div class="title-bar">
-      <h1 class="title">Booling✨</h1>
-    </div>
+	<div class="window">
+		<div class="title-bar">
+			<h1 class="title">Booling✨</h1>
+		</div>
 
-    <div class="details-bar details">
-      <router-link to="/">Home</router-link>
-      <a href="/blog">Blog</a>
-      <router-link to="/about">About</router-link>
-    </div>
+		<div class="details-bar details">
+			<router-link to="/">Home</router-link>
+			<a href="/blog">Blog</a>
+			<router-link to="/about">About</router-link>
+		</div>
 
-    <router-view></router-view>
-  </div>
+		<router-view></router-view>
+	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'App'
-})
+<script lang="ts" setup>
 </script>
 
-<style scoped></style>
+<style>
+body {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+#app {
+	@media screen {
+		width: 30rem;
+	}
+}
+
+.details {
+	padding: 0.5rem 1.3rem 0.5rem 1.3rem;
+}
+</style>
--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,41 +1,72 @@
 <template>
-  <div class="modeless-dialog content">
-    <div class="profile">
-      <div class="avatar">
-        <img class="avatar__image" alt="avatar" src="../assets/avatar.webp" />
-      </div>
-
-      <div class="intro">
-        <h1 class="name">bingling_sama</h1>
-        <span class="text">A high school student from China. Living in Wuhan, Hubei
-          Province.</span>
-        <span class="text">Email:
-          <a href="mailto:bingling_sama@qq.com">bingling_sama@qq.com</a></span>
-      </div>
-    </div>
-
-    <div class="self-intro">
-      <span>Self Introduction</span><br />
-      <span class="text">Hi there, this is bingling_sama, an interest-driven
-        programmer.</span>
-      <ul class="text">
-        <li>Web Engineer, using Vue.</li>
-        <li>Java & Kotlin user, JVM the best!</li>
-        <li>The sideline is Python.</li>
-        <li>Also learning Rust...</li>
-        <li>Work for LittleSkin Commspt.</li>
-        <li>A big fan of Minecraft.</li>
-      </ul>
-    </div>
-  </div>
+	<div class="modeless-dialog content">
+		<div class="profile">
+			<div class="avatar">
+				<img style="height: 80%;" alt="avatar" src="../assets/avatar.webp" />
+			</div>
+			<div class="info">
+				<span style="font-weight: bold;">bingling_sama</span>
+				<br />
+				<span class="text">A high school student from China. Living in Wuhan, Hubei Province.
+				</span>
+				<span class="text">Email:
+					<a href="mailto:bingling_sama@qq.com">bingling_sama@qq.com</a>
+				</span>
+			</div>
+		</div>
+		<div class="intro">
+			<span>Self Introduction</span><br />
+			<span class="text">Hi there, this is bingling_sama, an interest-driven
+				programmer.</span>
+			<ul class="text">
+				<li>Web Engineer, using Vue.</li>
+				<li>Java & Kotlin user, JVM the best!</li>
+				<li>The sideline is Python.</li>
+				<li>Also learning Rust...</li>
+				<li>Work for LittleSkin Commspt.</li>
+				<li>A big fan of Minecraft.</li>
+			</ul>
+		</div>
+	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'Intro'
-})
+<script lang="ts" setup>
 </script>
 
-<style scoped></style>
+<style scoped>
+.content {
+	padding: 0.5rem 0.5rem 0.5rem 0.5rem !important;
+}
+
+.profile {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+
+.avatar {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 8rem;
+	width: 8rem;
+}
+
+.info {
+	width: 20rem;
+	padding: .5rem 0 .5rem .5rem;
+}
+
+.intro {
+	width: 26rem;
+	padding: .5rem 0 0 1rem;
+}
+
+.intro ul {
+	padding-left: 2rem;
+}
+
+.text {
+	font-size: medium;
+}
+</style>
