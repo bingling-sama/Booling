7e92675d436db2a22be78273a1eeb7295b7e3fdf
bingling-sama <bingling_sama@qq.com>
2023-12-16 21:01:55 +0800

    🐞 fix(css): improve about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,26 +1,24 @@
 <template>
 	<div class="modeless-dialog about">
 		<h1 class="dialog-text">About</h1>
-		<span class="dialog-text">This is a webpage made with
-			<a href="https://vue3js.cn"> Vue 3</a> &
+		<span class="dialog-text">This is a webpage made with <a href="https://vue3js.cn"> Vue 3</a> &
 			<a href="https://cn.vitejs.dev"> Vite</a> &
-			<a href="https://github.com/sakofchit/system.css"> System.css</a>, deployed on
-			<a href="https://vercel.com"> Vercel</a>.
-			<br> You can find the source code of this page on
-			<a href="https://github.com/bingling-sama/Booling/">GitHub</a>.
-		</span><br>
+			<a href="https://github.com/sakofchit/system.css"> System.css</a>,
+			deployed on <a href="https://vercel.com"> Vercel</a>. <br />
+			You can find the source code of this page on
+			<a href="https://github.com/bingling-sama/Booling/">GitHub</a>. </span><br />
 		<span class="dialog-text">Love from China❤️</span>
 		<h5 class="dialog-text">&copy; 2020 bingling_sama</h5>
 	</div>
 </template>
 
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <style scoped>
 .about {
 	text-align: center;
-	line-height: 5vh;
-	padding: 1vh 1vw 1vh 1vw;
+	align-items: center;
+	line-height: 2.6rem;
+	padding: 0 0 0 0;
 }
 </style>
